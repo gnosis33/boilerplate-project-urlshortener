b4cb9fc09895bcd9b8d93d9a55f5adeab7f8428d
Brent Wise <brentjosephwise@gmail.com>
2024-10-09 11:15:20 -0700

    THIS IS A TEST...THIS BETTER WORK-2

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
-
-// Serve static files from /public
-app.use('/public', express.static(`${process.cwd()}/public`));
 
 // Serve index.html on root
 app.get('/', (req, res) => {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
