726885ac8218993bb24634137d7867a0ea94ede6
Brent Wise <brentjosephwise@gmail.com>
2024-10-09 11:56:16 -0700

    TEST-2

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use('/public', express.static(`${process.cwd()}/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
